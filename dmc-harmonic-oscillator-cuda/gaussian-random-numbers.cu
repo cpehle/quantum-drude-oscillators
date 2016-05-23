5465e18b453f7d0feb5eace6e5fa3adf7e7145f9
Kshitij Lauria <lauria@deshawresearch.com>
2016-05-23 17:15:28 -0400

    Print the generated numbers so I can look at them and plot them.

--- dmc-harmonic-oscillator-cuda/gaussian-random-numbers.cu
+++ dmc-harmonic-oscillator-cuda/gaussian-random-numbers.cu
@@ -1,20 +1,24 @@
 #include <curand_kernel.h>
 #include <curand_normal.h>
+
 #include <moderngpu/transform.hxx>
 #include <moderngpu/kernel_reduce.hxx>
 #include <moderngpu/memory.hxx>
+
 #include <cstdlib>
+#include <cassert>
 
 int main(int argc, char** argv) {
-
+  assert(argc == 2);
   mgpu::standard_context_t context;
 
-  int count = 100000000;
+  int count = 1000;
   int seed = atoi(argv[1]);
 
-  mgpu::mem_t<float2> outputs(1, context);
+  mgpu::mem_t<float2> outputs(count, context);
+  auto outputs_data = outputs.data();
 
-  mgpu::transform_reduce(
+  mgpu::transform(
     [=]MGPU_DEVICE(int index) {
       curandStatePhilox4_32_10_t state;
 
@@ -22,17 +26,15 @@ int main(int argc, char** argv) {
       uint4 result = curand4(&state);
 
       float2 yo = _curand_box_muller(result.x, result.y);
-      return yo;
-
-    }, count, outputs.data(), 
-      []MGPU_DEVICE(float2 a, float2 b) {
-        return make_float2(a.x + b.x, a.y + b.y);
-      }, context
+      outputs_data[index] = yo;
+    },
+    count,
+    context
   );
 
   std::vector<float2> host = from_mem(outputs);
   for(float2 p : host) {
-    printf("% 13.3e % 13.3e\n", p.x / count, p.y / count);
+    printf("% 13.3e % 13.3e\n", p.x, p.y);
   }
 
   return 0;
