f7a1d3f38f3ee8513ae63cbf62e41f80768228b1
Kshitij Lauria <lauria@deshawresearch.com>
2016-06-08 17:44:47 -0400

    Use transform_scan and store-iterators

--- dmc-cuda/resampling-dmc.cu
+++ dmc-cuda/resampling-dmc.cu
@@ -14,15 +14,14 @@ int main(int argc, char **argv) {
 
   mgpu::mem_t<float> weights(num_walkers, context);
   mgpu::mem_t<float> total_weight(1, context);
-  
+
+  /* Compute the weight for every walker. */
   auto weights_data = weights.data();
   mgpu::transform_reduce(
     [=]MGPU_DEVICE(uint index) {
-      //float random = gpu_random::uniforms<1>(uint4{index, 0, 0, 0}, uint2{seed, 0})[0];
-      float random = index*index;
-      //random *= random;
-      weights_data[index] = random;
-      return random;
+      float weight = index*index;
+      weights_data[index] = weight;
+      return weight;
     },
     num_walkers,
     total_weight.data(),
@@ -30,71 +29,54 @@ int main(int argc, char **argv) {
     context);
 
   float total_weight_host = mgpu::from_mem(total_weight)[0];
-  printf("%f\n", total_weight_host);
 
-  mgpu::transform(
-    [=]MGPU_DEVICE(uint index) {
-      weights_data[index] = weights_data[index]/total_weight_host;
-    },
-    num_walkers,
-    context);
-  
+  /* Compute the prefix-sum for weights. */
   mgpu::mem_t<float> should_be_one(1, context);
   mgpu::mem_t<float> weights_prefix_sum(num_walkers, context);
-  mgpu::scan(weights.data(), num_walkers, weights_prefix_sum.data(),
-             mgpu::plus_t<float>(), should_be_one.data(), context);
-
+  mgpu::transform_scan<float,mgpu::scan_type_t::scan_type_inc>(
+    [=]MGPU_DEVICE(uint index) {
+      return weights_data[index]/total_weight_host;
+    },
+    num_walkers,
+    weights_prefix_sum.data(),
+    mgpu::plus_t<float>(),
+    should_be_one.data(),
+    context);
+  
   float one = mgpu::from_mem(should_be_one)[0];
   printf("%f\n", one);
 
+  /*
+    as in, if you want to have 4 walkers, then use the points (0.2+x,0.4+x,0.6+x,0.8+x), where x is uniform between 0 and 0.2
+  */
   mgpu::mem_t<float> probabilities(num_walkers, context);
   auto probabilities_data = probabilities.data();
   mgpu::transform(
     [=]MGPU_DEVICE(uint index) {
-      float random = gpu_random::uniforms<1>(uint4{index, 0, 0, 0}, uint2{seed, 1})[0];
+      //float random = gpu_random::uniforms<1>(uint4{index, 0, 0, 0}, uint2{seed, 1})[0];
+      float random = float(index)/num_walkers;
       probabilities_data[index] = random;
     }, num_walkers, context);
 
   mgpu::mergesort(probabilities.data(), num_walkers, mgpu::less_t<float>(), context);
 
   mgpu::mem_t<int> indices(num_walkers, context);
-  mgpu::sorted_search<mgpu::bounds_upper>(probabilities.data(),
-                                          num_walkers,
-                                          weights_prefix_sum.data(),
-                                          num_walkers,
-                                          indices.data(),
-                                          mgpu::less_t<float>(),
-                                          context);
-
+  mgpu::sorted_search<mgpu::bounds_lower>(
+    probabilities.data(), num_walkers,
+    weights_prefix_sum.data(), num_walkers,
+    mgpu::make_store_iterator<int>([=]MGPU_DEVICE(uint parent_index, uint child_index) {
+        printf("%d %d\n", child_index, parent_index);
+      }),
+    mgpu::less_t<float>(),
+    context);
+  
   std::vector<float> ww = mgpu::from_mem(weights);
   std::vector<float> prefixes = mgpu::from_mem(weights_prefix_sum), probs = mgpu::from_mem(probabilities);
-  std::vector<int> inds    = mgpu::from_mem(indices);
+  //std::vector<int> inds    = mgpu::from_mem(indices);
 
   for(int ii = 0; ii < num_walkers; ++ii) {
-    printf("%f %f %f %d\n", ww[ii], prefixes[ii], probs[ii], inds[ii]);
+    printf("%f %f %f\n", ww[ii], prefixes[ii], probs[ii]);
   }
-  
-  
-  /*
-  
-  
-  auto numbers_data = numbers.data();
-  mgpu::transform(
-    [=]MGPU_DEVICE(uint index) {
-      numbers_data[index] = -float(index % 5);
-    }, 20, context);
-
-  std::vector<float> before = mgpu::from_mem(numbers);
-  for(auto xx : before)
-    printf("%f ", xx);
-  printf("\n");
-
-  mgpu::mergesort(numbers.data(), 20, mgpu::less_t<float>(), context);
-
-  std::vector<float> after = mgpu::from_mem(numbers);
-  for(auto xx : after)
-    printf("%f ", xx);
-  printf("\n");
-  */  
+ 
   return 0;
 }
