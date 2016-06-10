eafa5f170838eb0c2da0101aa8c96563a2aa2163
Kshitij Lauria <kslauria@gmail.com>
2016-06-09 21:27:50 -0400

    Fix bug in variance calculation; thermalize for 500 steps before collecting statistics; use doubles for accumulators

--- dmc-cuda/dmc-xenon-dimer.cu
+++ dmc-cuda/dmc-xenon-dimer.cu
@@ -3,6 +3,8 @@
 #include "qdo-dimer.hxx"
 
 int main(int argc, char** argv) {
+  uint thermalization_iters = 500;
+
   assert(argc == 4);
   uint niters = atoi(argv[1]);
   int target_num_walkers = atoi(argv[2]);
@@ -15,15 +17,18 @@ int main(int argc, char** argv) {
   TraditionalDMC<system> dd(target_num_walkers, context);
   dd.initialize();
 
-  float total_energy = 0.0, total_squared_energy = 0.0;
+  for(uint iter = 0; iter < thermalization_iters; ++iter)
+    dd.step(system::parameter_t{radial_distance});
+
+  double total_energy = 0.0, total_squared_energy = 0.0;
   for(uint iter = 0; iter < niters; ++iter) {
     float energy = dd.step(system::parameter_t{radial_distance});
     total_energy += energy;
     total_squared_energy += energy*energy;
   }
 
-  float energy_mean = total_energy/niters;
-  float energy_variance = total_squared_energy/niters - energy_mean;
+  double energy_mean = total_energy/niters;
+  double energy_variance = total_squared_energy/niters - energy_mean*energy_mean;
   
   printf("%f %f\n", energy_mean, energy_variance);  
   return 0;
